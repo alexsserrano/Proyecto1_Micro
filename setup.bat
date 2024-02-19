992db6404066a0da87d874d69bfde7cdcc60ebbc
=Adolfo Mariscal V <=itofito_8@hotmail.com>
2024-02-18 19:24:28 -0600

    Cambios al README y se agregan las instrucciones a seguir

--- setup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-rem Crear y activar el entorno virtual
-python -m venv venv
-venv\Scripts\activate
-
-rem Instalar dependencias
-pip install -r requirements.txt
--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Crear y activar el entorno virtual
-python -m venv venv
-source venv/bin/activate
-
-# Instalar dependencias
-pip install -r requirements.txt
