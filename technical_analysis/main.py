8ab2861e33965984bbfb89fdde10e6d05a8a0cec
Luisahum270 <89416666+Luisahum270@users.noreply.github.com>
2024-02-21 10:54:24 -0600

    cambiosluis

---
+++ technical_analysis/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import ta
+
+data5m=pd.read_csv("data/aapl_5m_train.csv")
+data1m=data.read_csv("data/aapl_1m_train.csv")
+data1d=pd.read_csv("data/aapl_1d_train.csv")
+data1h=data.read_csv("data/aapl_1h_train.csv")
+
+
